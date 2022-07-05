7aa58f680f85fbdbbe9de686889f06ec6da8cc41
cody <kenleybeast1@gmail.com>
2022-07-05 16:46:58 +0800

    Created Login Page React Js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Pages/LoginPage/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div className='hello'>
+      <Login></Login>
+      <h1> hello</h1>
     </div>
-  );
+  )
 }
-
 export default App;
