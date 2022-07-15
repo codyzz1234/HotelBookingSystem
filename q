011ab2c6c6916bbcc3d121e9907b128169048c5e
cody <kenleybeast1@gmail.com>
2022-07-15 21:12:11 +0800

    Fixed some layout problems in signuppage.jsx

---
+++ q
@@ -0,0 +1,47 @@
+[33mcommit b0eeede6d268fd0dc8c8d71d1014cb687d8b3810[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Fri Jul 15 18:45:19 2022 +0800
+
+    Added react router to sign up page
+
+[33mcommit 66d114ef0544309058ad1bce74b5f64d91abd578[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Fri Jul 15 18:07:35 2022 +0800
+
+    Created sign up age design and styling
+
+[33mcommit 5482ef9ca694e46b2298407f780952703c6d16d1[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Wed Jul 13 19:17:44 2022 +0800
+
+    Added firebase CLI
+
+[33mcommit b3d38e16433bbe05a6817699385870ac0b26dc81[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Thu Jul 7 18:48:05 2022 +0800
+
+    Added react firebase
+
+[33mcommit 3430b5c6d30029e8230bcd16dd3c7fdae01d1585[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Thu Jul 7 18:14:07 2022 +0800
+
+    Username and password gets cleared
+
+[33mcommit 9570ecd400ba6fb88da020b817a7bd403345d86f[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Thu Jul 7 17:59:28 2022 +0800
+
+    Added states for username as password
+
+[33mcommit 7aa58f680f85fbdbbe9de686889f06ec6da8cc41[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Tue Jul 5 16:46:58 2022 +0800
+
+    Created Login Page React Js
+
+[33mcommit f34d65b3e3ba530398cc473991a9767ce15912df[m
+Author: cody <kenleybeast1@gmail.com>
+Date:   Tue Jul 5 13:45:09 2022 +0800
+
+    Initialize project using Create React App
--- src/Pages/SignUpPage/SignUpPage.jsx
+++ src/Pages/SignUpPage/SignUpPage.jsx
@@ -1,55 +1,80 @@
+/*Required components*/
 import React from 'react'
 import "../../assets/Styles/SignUpPageStyle/SignUpStyle.css";
+/*images*/
 import SignUpLogo from "../../assets/Images/SignUpPageImages/SignUpImage.png"
-export const SignUpPage = () => {
-  return (
-    <>
-  <meta charSet="UTF-8" />
-  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link
-    rel="stylesheet"
-    href="../src/assets/Styles/SignUpPageStyle/SignUpStyle.css"
-  />
-  <link rel="stylesheet" href="../src/assets/Styles/bootstrap.css" />
-  <title>Document</title>
-  <div className="flex-container">
-    <div className="form-container">
-      <img
-        src = {SignUpLogo}
-        alt=""
-        className="img-fluid"
-      />
+import { useRef } from 'react';
 
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-          />
-          <small id="emailHelp" className="form-text text-muted">
-            We'll never share your email with anyone else.
-          </small>
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            className="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary signUpButton">
-          Submit
-        </button>
-      </form>
+export const SignUpPage = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+
+  const createUser = ()=>{
+    console.log("Signing utype")
+    console.log("Email is" + emailRef.current.value);
+    console.log("password is " + passwordRef.current.value);
+    emailRef = emailRef.current.value
+  }
+
+  return (
+  <>
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="../src/assets/Styles/SignUpPageStyle/SignUpStyle.css"
+    />
+    <link rel="stylesheet" href="../../" />
+    <link rel="stylesheet" href="../src/assets/Styles/bootstrap.css" />
+    <title>Document</title>
+    <div className="flex-container">
+      <div className="form-container"> 
+        <img
+          src = {SignUpLogo}
+          alt=""
+          className="img-fluid"
+        />
+
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              ref = {emailRef}
+            />
+            <small id="emailHelp" className="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              ref = {passwordRef}
+            />
+          </div>
+          <button 
+            type="submit"
+            className="btn btn-primary signUpButton"
+            onClick={(e)=>{
+              e.preventDefault();
+              createUser();
+            }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
   </>
 
   )
