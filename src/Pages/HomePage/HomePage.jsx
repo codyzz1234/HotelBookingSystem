8ae9bd94c83f0554de6e82aff70ed8e1d40d5404
cody <kenleybeast1@gmail.com>
2022-07-17 22:24:28 +0800

    added home page styling

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useRef } from 'react'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
+import "../../assets/Styles/HomePageStyle/StyleSheetTest.css"
 
 
 const HomePage = () => {
@@ -21,13 +23,32 @@ const HomePage = () => {
       }
   }
 
+  const addClassButton = useRef();
+
+  const addSomeClass = () =>{
+    addClassButton.current.className = "bayot";
+  }
+
   return(
+    <div className="container">
+
     <div className="button"
-          onClick={(e)=>{
-            handleLogout();
-          }}>
-      Logout
+              onClick={(e)=>{
+                handleLogout();
+              }}>
+          Logout
+        </div>
+
+        <button className="addClassButton"
+            ref = {addClassButton}
+            onClick = {(e)=>{
+              e.preventDefault();
+              addSomeClass();
+            }}>
+          addclass Test
+        </button>
     </div>
+    
   )
 
 
