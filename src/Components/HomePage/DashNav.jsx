cbc26a8c7d6db30ae2e99af604054f338ac497ef
cody <kenleybeast1@gmail.com>
2022-07-18 15:10:03 +0800

    Added Nav Bar Icons

---
+++ src/Components/HomePage/DashNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const DashNav = () => {
+  return (
+    
+  )
+}
+export default DashNav
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -4,6 +4,10 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
 import "../../assets/Styles/HomePageStyle/StyleSheetTest.css"
+import NavBar from '../../Components/HomePage/DashNav'
+import DashNav from '../../Components/HomePage/DashNav'
+import "../../assets//Styles//HomePageStyle//DashboardStyle.css"
+import "../../assets///Styles//bootstrap.css"
 
 
 const HomePage = () => {
@@ -13,7 +17,6 @@ const HomePage = () => {
   
   async function handleLogout() {
       setError("")
-
       try {
           await logout()
           navigate("/")
@@ -23,33 +26,30 @@ const HomePage = () => {
       }
   }
 
-  const addClassButton = useRef();
-
-  const addSomeClass = () =>{
-    addClassButton.current.className = "bayot";
-  }
 
   return(
-    <div className="container">
-
-    <div className="button"
-              onClick={(e)=>{
-                handleLogout();
-              }}>
-          Logout
-        </div>
-
-        <button className="addClassButton"
-            ref = {addClassButton}
-            onClick = {(e)=>{
-              e.preventDefault();
-              addSomeClass();
-            }}>
-          addclass Test
-        </button>
+    <div>
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../src/assets/Styles/HomePageStyle/DashboardStyle.css" />
+    <link rel="stylesheet" href="/src/assets/Styles/bootstrap.css" />
+    <title>Dashboard</title>
+    <div className="grid-container">
+      <div className="Nav-Bar-Container">
+        <DashNav></DashNav>
+      </div>
+      <div className="content">
+        <h1> Content</h1>
+      </div>
     </div>
+  </div>
+
+
     
   )
+
+  
 
 
 }
